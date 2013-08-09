296ec369b42f3adbf80ef82682a32f8f7ec4bb0d
Victor Ursan <victor.ursan@gmail.com>
2013-08-09 12:59:03 +0300

    adeed the MainClumsyViewController

---
+++ Clumsy/MainClumsyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainClumsyViewController.h
+//  Clumsy
+//
+//  Created by Victor Ursan on 8/9/13.
+//  Copyright (c) 2013 Victor Ursan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainClumsyViewController : UIViewController
+
+@end
---
+++ Clumsy/MainClumsyViewController.m
@@ -0,0 +1,27 @@
+//
+//  MainClumsyViewController.m
+//  Clumsy
+//
+//  Created by Victor Ursan on 8/9/13.
+//  Copyright (c) 2013 Victor Ursan. All rights reserved.
+//
+
+#import "MainClumsyViewController.h"
+
+@interface MainClumsyViewController ()
+
+@end
+
+@implementation MainClumsyViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
